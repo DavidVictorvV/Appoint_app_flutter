ea616ce8f746071d00d52c75d946bee9e0974634
DavidVictorvV <davidvictorvanvelden@gmail.com>
2022-07-11 13:52:17 +0300

    edit import

--- lib/auth_page/auth_pages/verify_email.dart
+++ lib/auth_page/auth_pages/verify_email.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../global_utils/utils.dart';
-import '../../main_page.dart';
+import '../../main_page_controller.dart';
 
 class VerifyEmailPage extends StatefulWidget {
   bool darkM;
